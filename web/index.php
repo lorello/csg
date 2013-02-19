da06353f66e47ff5e1861b8e0899ba2bde4f42ba
LoreLLo <lorello@openweb.it>
2013-02-19 17:20:22 +0100

    Initial import

---
+++ web/index.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+require '../src/Softec/Cloud/Storage.php';
+
+# to use Request object
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+// Turn on debugging
+// TODO: create index_dev.php for dev environment
+$app['debug'] = true;
+
+// TODO: separate this in bootstrap
+// Create the app instance
+$app = new Silex\Application();
+
+// TODO: separate these in src/controllers.php
+
+$app->get(
+    '/v1/files.get',
+    function () use ($app) {
+        $name = $request->headers->get('name');
+
+        $f = ObjectStorageFactory::load($name);
+
+        // TODO: throw an exception specific in preceding load
+        // then catch here and return a 404
+        if (!$f->exists()){
+            $app->error('404', "File $name is not present");
+        }
+
+        return $f->get();
+    }
+);
+
+
+$app->error(
+    function (\Exception $e, $code) use ($app) {
+        if ($app['debug']) {
+            return;
+        }
+
+        switch ($code) {
+            case 404:
+                $message = 'The requested page could not be found.';
+                break;
+            default:
+                $message = 'We are sorry, but something went terribly wrong.';
+        }
+        return new Response($message);
+    }
+);
+
+
+
+
+$app->run();
